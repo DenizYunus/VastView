dda5132c2c6fe4240db785ca8f7126d6f10276ca
Deniz Yunus Göğüş <58386750+DenizYunus@users.noreply.github.com>
2024-05-16 04:55:13 +0300

    Joystick Added

---
+++ Assets/Scripts/JoystickMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class JoystickMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.LThumbstickDown))
+        {
+            ClimbProvider.Instance.MoveBackward();
+        }
+        if (OVRInput.Get(OVRInput.RawButton.LThumbstickUp))
+        {
+            ClimbProvider.Instance.MoveForward();
+        }
+        if (OVRInput.Get(OVRInput.RawButton.LThumbstickLeft))
+        {
+            ClimbProvider.Instance.MoveLeft();
+        }
+        if (OVRInput.Get(OVRInput.RawButton.LThumbstickRight))
+        {
+            ClimbProvider.Instance.MoveRight();
+        }
+        if (OVRInput.Get(OVRInput.RawButton.RThumbstick))
+        {
+            ClimbProvider.Instance.ToggleFlyMode();
+        }
+        if (OVRInput.Get(OVRInput.RawButton.RThumbstickUp))
+        {
+            ClimbProvider.Instance.MoveUp();
+        }
+        if (OVRInput.Get(OVRInput.RawButton.RThumbstickDown))
+        {
+            ClimbProvider.Instance.MoveDown();
+        }
+    }
+}
