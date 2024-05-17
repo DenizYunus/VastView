a640d48fd6d54d8b6d69392c9ca89715b3a5c39f
Deniz Yunus Göğüş <58386750+DenizYunus@users.noreply.github.com>
2024-05-17 17:20:16 +0300

    Video Created, Joystick edited

--- Assets/Scripts/JoystickMovement.cs
+++ Assets/Scripts/JoystickMovement.cs
@@ -7,17 +7,21 @@ public class JoystickMovement : MonoBehaviour
         if (OVRInput.Get(OVRInput.RawButton.LThumbstickDown))
         {
             ClimbProvider.Instance.MoveBackward();
+            ClimbProvider.Instance.MoveBackward();
         }
         if (OVRInput.Get(OVRInput.RawButton.LThumbstickUp))
         {
+            ClimbProvider.Instance.MoveForward();
             ClimbProvider.Instance.MoveForward();
         }
         if (OVRInput.Get(OVRInput.RawButton.LThumbstickLeft))
         {
             ClimbProvider.Instance.MoveLeft();
+            ClimbProvider.Instance.MoveLeft();
         }
         if (OVRInput.Get(OVRInput.RawButton.LThumbstickRight))
         {
+            ClimbProvider.Instance.MoveRight();
             ClimbProvider.Instance.MoveRight();
         }
         if (OVRInput.GetDown(OVRInput.RawButton.RThumbstick))
@@ -27,9 +31,11 @@ public class JoystickMovement : MonoBehaviour
         if (OVRInput.Get(OVRInput.RawButton.RThumbstickUp))
         {
             ClimbProvider.Instance.MoveUp();
+            ClimbProvider.Instance.MoveUp();
         }
         if (OVRInput.Get(OVRInput.RawButton.RThumbstickDown))
         {
+            ClimbProvider.Instance.MoveDown();
             ClimbProvider.Instance.MoveDown();
         }
     }
